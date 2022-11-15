04f6f355123a09a3de19713ca82f7220a4bfffba
AlwaysBulking <adrianoddonati@hotmail.it>
2022-11-15 17:53:12 +0100

    Added html page and scripts

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const port = 3000;
 const serverStartedMessage = `Express Server running on port ${port}`
 
 const HOME = '/'
+const LOGIN = '/login'
+
+app.use(express.static(__dirname + '/public'))
 
 app.get(HOME, (req, res) => {
-    //TODO: restituisci la pagina in cui inserire mail per registrarsi
+    res.sendFile(path.join(__dirname, '/public/login.html'))
+})
+
+app.post(LOGIN, (req, res) => {
+    req.body
 })
 
 app.listen(port, () => {
