c29133ed0f33f6845dd1ee68e7c9a748133d8bc4
AlwaysBulking <adrianoddonati@hotmail.it>
2022-11-15 18:09:03 +0100

    Revert "Added html page and scripts"
    This reverts commit 04f6f355123a09a3de19713ca82f7220a4bfffba.

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require('express')
-const path = require('path')
 
 const app = express()
 const port = 3000;
 const serverStartedMessage = `Express Server running on port ${port}`
 
 const HOME = '/'
-const LOGIN = '/login'
-
-app.use(express.static(__dirname + '/public'))
 
 app.get(HOME, (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/login.html'))
-})
-
-app.post(LOGIN, (req, res) => {
-    req.body
+    //TODO: restituisci la pagina in cui inserire mail per registrarsi
 })
 
 app.listen(port, () => {
