31d41d56d8cec6d9893b057051bd0c38bd83e070
AlwaysBulking <adrianoddonati@hotmail.it>
2022-11-17 18:05:32 +0100

    Continued test of key functions

--- public/scripts/passkey.js
+++ public/scripts/passkey.js
@@ -1,18 +1,19 @@
+
 async function createPassKey() {
-  const relyingPartyId = ''
-  const relyingPartyName = ''
-  const userId = ''
-  const userName = ''
-  const displayName = ''
+  const relyingPartyId = '192.168.1.8'
+  const relyingPartyName = '192.168.1.8'
+  const userId = new ArrayBuffer(8)
+  const userName = document.getElementById('username')
+  const displayName = document.getElementById('username')
   // Availability of `window.PublicKeyCredential` means WebAuthn is usable.
-  if (window.PublicKeyCredential &&
+  if (true /*window.PublicKeyCredential &&
     PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable &&
-    PublicKeyCredential.isConditionalMediationAvailable) {
+    PublicKeyCredential.isConditionalMediationAvailable*/) {
     // Check if user verifying platform authenticator is available.
     Promise.all([
-      PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable(),
-      PublicKeyCredential.isConditionalMediationAvailable,
-    ]).then(async results => {
+      /*PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable(),
+      PublicKeyCredential.isConditionalMediationAvailable(),
+    */]).then(async results => {
       if (results.every(r => r === true)) {
         const publicKeyCredentialCreationOptions = {
           challenge: new Uint8Array([0]),
@@ -27,7 +28,7 @@ async function createPassKey() {
           },
           pubKeyCredParams: [{ alg: -7, type: "public-key" }, { alg: -257, type: "public-key" }],
           excludeCredentials: [{
-            id: userid,
+            id: userId,
             type: 'public-key',
             transports: ['internal'],
           }],
@@ -38,12 +39,15 @@ async function createPassKey() {
           timeout: 30000
         };
 
-        const credential = await navigator.credentials.create({
+        let credentialContainer = window.navigator
+        credentialContainer.credentials.create({
           publicKey: publicKeyCredentialCreationOptions
-        });
+        }).then(console.log('evviva'));
 
         // Encode and send the credential to the server for verification.
       }
     });
+  } else { // Passkeys not supported
+    console.log('not supported')
   }
 }
