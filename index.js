0a83e9e43682eccc60e29659d82ab329a5ececc3
TamaZ9 <lukepoppantino@gmail.com>
2022-11-22 16:12:55 +0100

    Minor changes

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const port = 3000;
 const serverStartedMessage = `Express Server running on port ${port}`
 
 const HOME = '/'
-const LOGIN = '/login'
+const LOGIN = HOME + 'login'
+const PUBLIC = HOME + 'public'
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + PUBLIC))
 
 app.get(HOME, (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/login.html'))
+    res.sendFile(path.join(__dirname, PUBLIC + '/login.html'))
 })
 
 app.post(LOGIN, (req, res) => {
