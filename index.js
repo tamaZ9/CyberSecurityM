f89acc3fb00d22b83a0c3cf68b824a575cb408e4
TamaZ9 <lukepoppantino@gmail.com>
2022-11-22 16:56:28 +0100

    Implemented auto switch to Https protocol for every request

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const https = require('https')
+const http = require('http')
 const fs = require('fs')
 
 const app = express()
-const port = 3000;
+const port = 443;
 const serverStartedMessage = `Express Server running on port ${port}`
 
 const HOME = '/'
@@ -12,6 +13,8 @@ const LOGIN = HOME + 'login'
 const PUBLIC = HOME + 'public'
 
 app.use(express.static(__dirname + PUBLIC))
+
+app.all('*', switchToHttps)
 
 app.get(HOME, (req, res) => {
     res.sendFile(path.join(__dirname, PUBLIC + '/login.html'))
@@ -21,6 +24,8 @@ app.post(LOGIN, (req, res) => {
     req.body
 })
 
+
+http.createServer(app).listen(80)
 https.createServer(
     {
         key: fs.readFileSync("key.pem"),
@@ -30,3 +35,11 @@ https.createServer(
 ).listen(port, () => {
     console.log(serverStartedMessage)
 })
+
+function switchToHttps(req, res, next){
+    if (req.secure){
+        return next()
+    }
+
+    res.redirect('https://' + req.hostname + req.url)
+}
